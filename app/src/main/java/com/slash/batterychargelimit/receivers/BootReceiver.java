f2ffe92708030dc51c100246e0f561d2c581e70e
Michael Lux <michi.lux@gmail.com>
2017-08-16 15:16:30 +0200

    fixed issue with service starting on boot complete event when disabled

--- app/src/main/java/com/slash/batterychargelimit/receivers/BootReceiver.java
+++ app/src/main/java/com/slash/batterychargelimit/receivers/BootReceiver.java
@@ -19,7 +19,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            context.startService(new Intent(context, ForegroundService.class));
+            SharedMethods.startService(context);
         }
     }
 }
