77d34ff39dacd1af01bc5a5adbc3ed585ce999a0
Sri Harsha Arangi <harsha1397@yahoo.com>
2018-08-18 12:01:33 +0530

    Added splash screen

---
+++ app/src/main/java/com/slash/batterychargelimit/activities/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.slash.batterychargelimit.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
